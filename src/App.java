4d95b7294728c96a437ce4d7dcfa2d50e630f7f1
Philip Juan Hadikusuma <philiphadikusuma2007@gmail.com>
2026-06-09 19:23:17 +0700

    coba

--- src/App.java
+++ src/App.java
@@ -1,7 +1,4 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, Nama saya PPhilip!");
-        System.out.println("Hello, World!");
     }
 }
